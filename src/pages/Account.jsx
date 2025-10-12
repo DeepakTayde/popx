bf23704130d0ca896b135008e8de9248f820615a
deepaktayde <deepaktayde559@gmail.com>
2025-10-13 00:18:48 +0530

    Account page basic structure implemented

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Account = () => {
   return (
-    <div className='flex flex-col '></div>
+    <div className='flex flex-col justify-start items-start w-full h-full'>
+      <div className='w-full'><h3 className='text-gray-900 font-Inter font-bold bg-white py-5'>Account Settings</h3></div>
+      <div className='w-full h-[1px] bg-gray-300'/>
+      <div className='w-full h-full flex flex-col'>
+      
+      </div>
+
+    </div>
   )
 }
 
