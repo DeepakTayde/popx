67bc64deb6749ffffb80322bcbc0d551a1be7d4e
deepaktayde <deepaktayde559@gmail.com>
2025-10-15 04:11:48 +0530

    fixing COntext bugs

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -1,12 +1,10 @@
 // src/context/UserProvider.jsx
 import React, { useState } from "react";
-import { UserContext } from "./userContext";
+import { UserContext } from "./UserContext";
 
 
 export default function UserProvider({ children }) {
   const [user, setUser] = useState(null);
-  console.log("Context user", user);
-
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
